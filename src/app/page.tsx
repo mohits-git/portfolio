9b2bb0ac9725a19d856fdb26bc976a3a1809859f
mohits-git <mohitkhatri2805@gmail.com>
2024-07-07 22:35:49 +0530

    refac: clean up terminal and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,7 @@
-'use client';
-
-import CustomModal from "@/components/custom-modal";
-import { useModal } from "@/providers/modal";
-
 export default function Home() {
-  const { setOpen } = useModal();
-  const handleClick = () => {
-    setOpen((
-      <CustomModal>
-        <div className="rounded-lg bg-black/50 text-white shadow-black/30 min-w-[500px] min-h-[500px]">
-          Hellew from modal
-        </div>
-      </CustomModal>
-    ));
-  }
   return (
     <div className="w-screen h-full flex flex-col justify-center items-center">
-      Home Page
-      <button
-        type="button"
-        onClick={() => handleClick()}
-        className="bg-black text-white rounded-md"
-      >
-        Open Modal
-      </button>
+      Mohit Khatri
     </div>
   );
 }
--- src/components/terminal/index.tsx
+++ src/components/terminal/index.tsx
@@ -8,20 +8,21 @@ type Props = {
 const Terminal: React.FC<Props> = () => {
   return (
     <div className="fixed bottom-2 left-0 w-full flex justify-center">
-      <div className="relative min-h-[65px] h-full max-w-5xl w-full p-1 md:px-2 flex flex-col border rounded-md underline-offset-4 decoration-gray-300">
-        {/* ------------------TerminalIcon--------------------------- */}
+      <div className="relative min-h-[65px] h-full max-w-5xl w-full py-2 px-1 md:px-2 flex flex-col space-y-1 border rounded-md underline-offset-4 decoration-gray-300">
         <div className="absolute top-2 right-2">
           <SquareTerminal width={30} height={30} />
         </div>
-        {/* ---------------------------------------------------------- */}
 
-        <p>Last login: Tue Jul  2 13:03:32 on ttys001</p>
         <p className="text-gray-500">run &apos;help&apos; to get information about the commands</p>
 
-        <div className="relative flex items-center">
+        <div className="relative flex items-center w-full">
           <ChevronRight width={20} height={20} />
-          <textarea name="terminal" id="terminal" className="w-full bg-transparent border-none outline-none focus:outline-none h-[1.5rem]">
-          </textarea>
+          <input
+            type="text"
+            name="terminal"
+            id="terminal"
+            className="w-full bg-transparent border-none outline-none focus:outline-none h-[1.5rem]"
+          />
         </div>
 
       </div>
