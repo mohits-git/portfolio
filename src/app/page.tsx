637980f3241e14df52a2312fa6a9a4112d86a593
mohits-git <mohitkhatri2805@gmail.com>
2024-07-08 22:21:59 +0530

    feat: add landing page animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,38 @@
+'use client';
+import { motion } from "framer-motion";
+
 export default function Home() {
   return (
     <div className="w-screen h-full flex flex-col justify-center items-center">
-      Mohit Khatri
+      <motion.div
+        animate={{
+          y: ["100%", "0%"],
+        }}
+        transition={{
+          duration: 0.5,
+          easings: "easeIn",
+        }}
+        className="text-[10rem] font-[900]"
+      >
+        Mohit&apos;s Site
+      </motion.div>
+      <div
+        className="text-[10rem] font-[900]"
+      >
+        Mohit&apos;s Site
+      </div>
+      <motion.div
+        animate={{
+          y: ["-100%", "0%"],
+        }}
+        transition={{
+          duration: 0.5,
+          easings: "easeIn",
+        }}
+        className="text-[10rem] font-[900] m-0 p-0"
+      >
+        Mohit&apos;s Site
+      </motion.div>
     </div>
   );
 }
