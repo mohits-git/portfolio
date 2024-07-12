c1bd28c76f82be9093a016baf092d2c6d4841f42
mohits-git <mohitkhatri2805@gmail.com>
2024-07-12 18:54:55 +0530

    fix: add z-index to terminal comp

--- src/components/terminal/index.tsx
+++ src/components/terminal/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Terminal: React.FC<Props> = () => {
   return (
-    <div className="fixed bottom-2 left-0 w-full flex justify-center">
+    <div className="fixed bottom-2 left-0 w-full flex justify-center z-10">
       <div className="mx-2 relative min-h-[65px] h-full max-w-5xl w-full py-2 px-1 md:px-2 flex flex-col space-y-1 border rounded-md underline-offset-4 decoration-gray-300 bg-black">
         <div className="absolute top-2 right-2">
           <SquareTerminal width={30} height={30} />
